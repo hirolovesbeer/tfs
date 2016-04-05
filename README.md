9fc1be071796a1c1f0c020c140f421415f65780b
Hiroshi ABE <hirolovesbeer@gmail.com>
2016-04-05 15:59:26 +0900

    add test framework

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# TFS
-PoC of Transparent File System
+# TFS(Transparent File System)
+This software is PoC(Proof of Concept) of Transparent File System.
+
+# dependency software
+
+- This software made by python 2.7 using anaconda packages
+- HDFS3(conda install hdfs3 -c dask)
+
+
+# test
+```python:setup.py
+$ python setup.py test
+```
--- test/test_hdfs_wrapper.py
+++ test/test_hdfs_wrapper.py
@@ -1,10 +1,21 @@
 import unittest
 
-# from hdfs_wrapper import tfs
 from tfs import hdfs_wrapper
 
-class TestTransparentFileSystem():
+class TestTransparentFileSystem(unittest.TestCase):
+	def setUp(self):
+		self.tfs = hdfs_wrapper.TransparentFileSystem()
+	
 	def test_init(self):
+		self.assertIsInstance(self.tfs, hdfs_wrapper.TransparentFileSystem)
+
+	def test_exists(self):
+		self.assertTrue(self.tfs.exists('/tmp'))
+
+	def test_cat(self):
+		return
+
+	def test_chmod(self):
 		return
 
 if __name__ == '__main__':
